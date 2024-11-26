1cb7fc7863af45c8a5b6db5cfb4574dd864c9d43
Aynaz Heydariafshar <aynaz@arianalabs.io>
2024-11-26 16:00:35 +0330

    FEAT : add new dark ui for cards form

--- src/components/ui/card-title-form.tsx
+++ src/components/ui/card-title-form.tsx
@@ -4,8 +4,8 @@ import { CardTitleFormProps } from "../../types/card-title-form-props";
 export function CardTitleForm({ title, children }: CardTitleFormProps) {
   return (
     <div className="mb-4">
-      <div className="border rounded-md p-2 flex flex-col gap-2">
-        <h1 className="text-white text-3xl font-bold m-2">{title}</h1>
+      <div className="border min-h-80 rounded-lg p-7 flex flex-col bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        <h1 className="text-white text-3xl font-bold">{title}</h1>
         {children}
       </div>
     </div>
--- src/pages/user-information.tsx
+++ src/pages/user-information.tsx
@@ -11,30 +11,32 @@ const contactMethodsItems = [
 export default function UserInformation() {
   return (
     <div className="bg-gradient-to-r from-[#05262b] via-[#1d434a] to-[#aa56fd] min-h-screen p-20">
-      <CardTitleForm title="User Information Form">
-        <div className="flex gap-2">
-          <Input label="First Name" name="firstName" type="text" />
-          <Input label="Last Name" name="lastName" type="text" />
-          <Input label="Address" name="address" type="text" />
-        </div>
-        <div className="flex p-2">
-          <Radio
-            radioItems={contactMethodsItems}
-            title="Preferred Contact Method"
-            name="preferredContactMethod"
-          />
-        </div>
-        <div className="flex gap-2">
-          <Input label="Phone Number" name="phoneNumber" type="number" />
-          <Input label="Email" name="email" type="email" />
-        </div>
-      </CardTitleForm>
-      <CardTitleForm title="Experience Section">
-        <div className="flex gap-2">
-          <Input label="Experience" name="experience" type="text" />
-          <Input label="Duration(months)" name="duration" type="number" />
-        </div>
-      </CardTitleForm>
+      <div className="flex items-center justify-center gap-2">
+        <CardTitleForm title="User Information Form">
+          <div className="flex gap-2">
+            <Input label="First Name" name="firstName" type="text" />
+            <Input label="Last Name" name="lastName" type="text" />
+            <Input label="Address" name="address" type="text" />
+          </div>
+          <div className="flex p-2">
+            <Radio
+              radioItems={contactMethodsItems}
+              title="Preferred Contact Method"
+              name="preferredContactMethod"
+            />
+          </div>
+          <div className="flex gap-2">
+            <Input label="Phone Number" name="phoneNumber" type="number" />
+            <Input label="Email" name="email" type="email" />
+          </div>
+        </CardTitleForm>
+        <CardTitleForm title="Experience Section">
+          <div className="flex flex-col gap-2">
+            <Input label="Experience" name="experience" type="text" />
+            <Input label="Duration(months)" name="duration" type="number" />
+          </div>
+        </CardTitleForm>
+      </div>
       <Button />
     </div>
   );
